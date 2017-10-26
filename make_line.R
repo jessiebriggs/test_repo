abaf62e52005f578bf8fd7cc8ca703c0419da6ec
jessiebriggs <jb1424@wildcats.unh.edu>
2017-10-25 21:40:16 -0400

    added quadratic

--- make_line.R
+++ make_line.R
@@ -3,9 +3,13 @@
 
 x <- -10:10
 b <- -2
+b2 <- 4
 a <- 100
 y <- b * x + a
+y2 <- b2*x^2 + b*x + a 
 
 plot(x, y, xlab = "independent", ylab = "dependent")
 
 plot(x, y, xlab = "independent", ylab = "dependent", type = "l")
+
+plot(x, y2, xlab = "independent", ylab = "dependent", type = "l")
